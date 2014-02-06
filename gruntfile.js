be5475a3a2d41cfd59a44509113fcc7c37a4eafe
Jmeas <jellyes2@gmail.com>
2014-02-06 17:16:50 -0500

    Added travis yml, code coverage

--- gruntfile.js
+++ gruntfile.js
@@ -23,18 +23,35 @@ module.exports = function( grunt ) {
       // }
     },
 
-    mocha: {
-      options: {
-        run: true
-      },
-      main: {
-        src: [ 'tests/**/*.html' ]
+    // Starts a server for running tests in the browser
+    connect: {
+      mocha: {
+        options: {
+          base: [ './', 'tests' ],
+          debug: true,
+          open: true,
+          keepalive: true
+        }
       }
-    }
+    },
+
+    blanket_mocha : {    
+      test: {
+        src: ['tests/index.html'],                
+        options: {    
+          threshold: 50,
+          globalThreshold: 65,
+          log: true,
+          logErrors: true
+        }                
+      }
+          
+      }
 
   });
 
-  grunt.registerTask( 'test', [ 'jshint', 'mocha' ] );
+  grunt.registerTask( 'browser-test', [ 'connect' ] );
+  grunt.registerTask( 'test', [ 'jshint', 'blanket_mocha' ] );
   grunt.registerTask( 'default', [ 'jshint', 'mocha', 'uglify' ] );
 
 };
